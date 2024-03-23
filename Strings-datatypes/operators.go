f0a3950ef9ca5a0ae7a98ecc8dcf4816ed80e6c0
Esta-10 <164031308+Esta-10@users.noreply.github.com>
2024-03-23 20:24:05 +0300

    Learning Operators
    Modulus, addition, division, subtration

---
+++ Strings-datatypes/operators.go
@@ -0,0 +1,66 @@
+// // // package main
+// // // import "fmt"
+
+// // // func main() {
+
+// // //   num1 := 6
+// // //   num2 := 2
+
+// // //   // + adds two variables
+// // //   sum := num1 + num2
+// // //   fmt.Printf("%d + %d = %d\n", num1, num2, sum)
+
+// // //   // - subtract two variables
+// // //   difference := num1 - num2
+// // //   fmt.Printf("%d - %d = %d\n",num1, num2,  difference)
+
+// // //   // * multiply two variables
+// // //   product := num1 * num2
+// // //   fmt.Printf("%d * %d is %d\n",num1, num2,  product)
+
+// // // }
+
+// // package main
+// // import "fmt"
+
+// // func main() {
+
+// //   num1 := 11
+// //   num2 := 4
+
+// //   // / divide two integer variables
+// //   quotient := num1 / num2
+// //   fmt.Printf(" %d / %d = %d\n", num1, num2, quotient)
+
+// // // }
+
+// package main
+// import "fmt"
+
+// func main() {
+
+//   num1 := 11.0
+//   num2 := 4.0
+
+//   // / divide two floating point variables
+//   result := num1 / num2
+//   fmt.Printf(" %g / %g = %g\n", num1, num2, result)
+
+// }
+package main
+
+import "fmt"
+
+func main() {
+
+	num := 5
+
+	// increment of num by 1
+	num++
+	fmt.Println(num) // 6
+
+	// decrement of num by 1
+	num--
+	fmt.Println(num) // 4
+
+}
